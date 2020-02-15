b7d48ad8348c37b092910c22236d95bfe08ccd15
jarim6 <jari.mutanen@gmail.com>
2020-02-15 15:36:26 +0200

    Fix all

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
 
 const axios = require('axios');
-    axios.get('https://fanuc-robot-http-server.herokuapp.com/')
-    .then((res) => {
-    console.log(res);
- });
 
- /**
 const start_time_stamp= new Date();
-const time_stamp = new Date();
-const delta = time_stamp - start_time_stamp;
- */
 
-const regexp = '/Joint/g';
+const main_loop = () => {
+setTimeout(() => {
+
+axios.get('https://fanuc-robot-http-server.herokuapp.com/')
+    .then((res) => {
+   
+const time_stamp = new Date();
+
+const delta = time_stamp - start_time_stamp;
+
+
+const regexp = 'Joint[ ]+[0-9]:[ ]+([ ][0-9]+.[0-9])';
+//const regexp = 'Joint[ ]?[-+]?[0-9]+[-,]*[+-]?[0-9]+';
+//const regexp = 'Joint[ ]';
+
 let joints = [];
 let matches = res.data.matchAll(regexp);
 let count = 0;
-    for (const match of matches) {
-    count++;
-    if (count > 6) break;
-    const value = parseFloat(match[1]);
-    joints.push(value);
+for (const match of matches) {
+count++;
+if (count > 6) break;
+const value = parseFloat(match[1]);
+joints.push(value);    
+  
 }
+console.log(joints)
+console.log(time_stamp, joints, delta, 'ms');
+
+});
+
+
+
+
 //Luo ohjelmaan luuppi, joka pyörii ikuisesti
 
-const main_loop = () => {
-    setTimeout(() => {
-    //Koodi
-    main_loop();
-    }, 10);
+main_loop();
+  }, 1000);
 }
+
 main_loop();
